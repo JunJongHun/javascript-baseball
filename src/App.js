672cda18c63d900d2dbeab042afb75a36a85854f
JunJongHun <minn4072@gmail.com>
2022-11-07 22:16:56 +0900

    feat : 컴퓨터 숫자 -> 1에서 9까지 서로 다른 임의의 3자리 수를 반환하는 함수

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+const MissionUtils = require("@woowacourse/mission-utils");
 class App {
-  play() {}
+  constructor() {
+    this.computerNumber = [];
+  }
+
+  play() {
+    this.computerNumber = this.getComputerNumber();
+
+    return;
+  }
+
+  getComputerNumber() {
+    return MissionUtils.Random.pickUniqueNumbersInRange(1, 9, 3); // [1,2,3]
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
